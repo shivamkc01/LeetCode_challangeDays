f819370a493ca6b947f83c5add5fe2ed2199beaf
Shivam Chhetry <39437051+shivamkc01@users.noreply.github.com>
2021-10-10 05:08:11 +0530

    Update LeetCode-Day-13.py

--- LeetCode-Day-13.py
+++ LeetCode-Day-13.py
@@ -1,5 +1,16 @@
 # Ouestion -> https://leetcode.com/problems/product-of-array-except-self/
+
 # Hint :- make two passes, first in-order, second in-reverse, to compute products
+
+#[1,2,3,4] -> Our orginal list
+#[1,1,2,6] -> left prefix
+#[1,4, 12, 24] -> right postfix
+#[24, 12, 4, 1] -> reverse right postfix list
+# multiply reverse right with left prefix list
+#[24*1, 12*1, 4*2, 6*1]
+#[24, 12, 8, 6] -> Our Output list->return
+
+##################################################################
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
@@ -12,3 +23,21 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+###################################################################
+#################### Other method is:##############################
+class Solution:
+    def productExceptSelf(nums: list[int]) -> list[int]:
+	output = [1]
+	for i in range(len(nums)-1, 0, -1):
+		output.append(output[-1]*nums[i])
+		print(output)
+	output = output[::-1]
+	print(output)
+	left = 1
+	for i in range(len(nums)):
+		output[i] = output[i]*left
+		print(output[i])
+		left *= nums[i]
+		print(left)
+	return output
+        
