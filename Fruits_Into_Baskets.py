e16f92f208daee9da80d323269f5517d51417d79
Shivam Chhetry <39437051+shivamkc01@users.noreply.github.com>
2021-10-11 20:13:31 +0530

    Add files via upload

---
+++ Fruits_Into_Baskets.py
@@ -0,0 +1,23 @@
+# Question link -> https://leetcode.com/problems/fruit-into-baskets/
+
+# Time Complexity ---> O(n)
+# Space Complexity --> O(1)
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        
+        start = 0
+        end = 0
+        d = {}
+        max_len = 0
+        while end < len(fruits):
+            d[fruits[end]] = end
+            if len(d) >= 3:
+                min_val = min(d.values())
+                del d[fruits[min_val]]
+                start = min_val + 1
+            max_len = max(max_len, end - start + 1)
+            end += 1
+        return max_len
+            
+
+                
